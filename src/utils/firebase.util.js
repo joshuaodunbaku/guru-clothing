78f5ddf4c5d5c6ae0ffb39a538e23c085d4f6ecc
Guru <joshuaodunbaku@gmail.com>
2022-07-27 13:46:52 +0100

    added an onAuthStageChange observer handler and a signOutUser handler with firebase auth signOut method

--- src/utils/firebase.util.js
+++ src/utils/firebase.util.js
@@ -6,14 +6,17 @@ import {
     GoogleAuthProvider,
     signInWithRedirect,
     createUserWithEmailAndPassword,
-    signInWithEmailAndPassword
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
 } from 'firebase/auth';
 
 import {
   getFirestore,
   doc,
   getDoc,
-  setDoc
+  setDoc,
+  Timestamp
 } from "firebase/firestore"
 
 // Your web app's Firebase configuration
@@ -44,6 +47,7 @@ export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googlePro
 
 export const db = getFirestore();
 
+// S I G N   I N 
 // a helper function that calls another function to creates a user profile with email and password
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
@@ -58,6 +62,23 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
   return await signInWithEmailAndPassword(auth, email, password);
 }
 
+// S I G N   O U T 
+export const signOutUser = async () => await signOut(auth)
+
+// A U T H E N T I C A T I O N   S T R E A M   M O N I T O R
+export const onAuthStateChangedListener = (callback) => 
+  onAuthStateChanged(auth, callback);
+  /**
+   * {
+   *  next: callback,
+   *  error: errorCallback
+   *  complete: completeCallback
+   *  }
+   */
+
+
+// C R E A T E   U S E R   D O C   O F   F I R E B A S E
+// creates user document inside the firestore storage
 export const createUserDocumentFromAuth = async (userAuth, additionalInfo={}) => {
   if (!userAuth) return;
 
@@ -71,7 +92,8 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInfo={}) =>
 
   if (!userSnapshot.exists()) {
     const {displayName, email} = userAuth;
-    const createdAt = new Date();
+    // const createdAt = new Date();
+    const createdAt = Timestamp.fromDate(new Date());
 
     try {
       await setDoc(userDocRef, {
