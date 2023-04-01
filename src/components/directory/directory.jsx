5cfd52c6f7c2994c398c5f0262ad123f4d849944
joshuaodunbaku <joshuaodunbaku@gmail.com>
2023-04-01 23:20:33 +0100

    added redirect for netlify

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -13,7 +13,7 @@ const categories = [
     },
     {
         "id": 2,
-        "title": "Hackets",
+        "title": "Jackets",
         "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png",
         "route": "shop/jackets",
     },
